6484696b155d5356c7cc2b1dd76704a05834e0d1
furkanartar <asosyalgenctvtr@gmail.com>
2021-06-19 17:37:33 +0300

    All structure has been re-created.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FinalDemo'`, () => {
+  it(`should have as title 'northwind'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FinalDemo');
+    expect(app.title).toEqual('northwind');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('FinalDemo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('northwind app is running!');
   });
 });
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,22 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
-  product1:any={productId:1, productName:"Bardak", categoryId:1, unitPrice:5}
-  product2:any={productId:2, productName:"Laptop", categoryId:1, unitPrice:5}
-  product3:any={productId:3, productName:"Mouse", categoryId:1, unitPrice:5}
-  product4:any={productId:4, productName:"Keyboard", categoryId:1, unitPrice:5}
-  product5:any={productId:5, productName:"Camera", categoryId:1, unitPrice:5}
-
-  products = [this.product1, this.product2,this.product3,this.product4,this.product5]
-  constructor() { }
+  products: Product[] = [];
+  dataLoaded = false;
+  filterText="";
+  
+  constructor(private productService:ProductService, 
+    private activatedRoute:ActivatedRoute, 
+    private toastrService:ToastrService,
+    private cartService:CartService) {}
 
   ngOnInit(): void {
+      this.activatedRoute.params.subscribe(params=>{
+        if(params["categoryId"]){
+          this.getProductsByCategory(params["categoryId"])
+        }else{
+          this.getProducts()
+        }
+      })
   }
 
+  getProducts() {
+    this.productService.getProducts().subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded = true;
+    })   
+  }
+
+  getProductsByCategory(categoryId:number) {
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded = true;
+    })   
+  }
+
+  addToCart(product:Product){
+      if(product.productId===1){
+        this.toastrService.error("Hata","Bu ürün sepete eklenemez")
+      }else{
+        this.toastrService.success("Sepete eklendi",product.productName)
+        this.cartService.addToCart(product);
+      }
+   
+  }
 }
